81be295586b738e92aa74c70b636bcf8dfce85de
tuankiet116 <kietdat1612000@gmail.com>
2024-05-30 02:05:22 +0700

    feat: prepare data to export and fix bug

--- app/Services/Admin/ExportService.php
+++ app/Services/Admin/ExportService.php
@@ -4,6 +4,10 @@ namespace App\Services\Admin;
 
 use App\Exports\AggregateRevenueDaily;
 use App\Models\BookModel;
+use App\Models\EventModel;
+use App\Models\EventSeatClassModel;
+use App\Models\TicketClassModel;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\DB;
 
 class ExportService
@@ -14,14 +18,56 @@ class ExportService
 
     public function exportReportAggregateRevenue(array $data)
     {
+        $eventIds = data_get($data, "events");
         $startDate = data_get($data, "start_date");
         $endDate = data_get($data, "end_date");
-        $bookings = BookModel::with(["client", "seat", "event"])
-            ->whereDate("created_at", ">=", $startDate)
-            ->whereDate("created_at", "<=", $endDate)
-            ->where("start_pending", null)
-            ->whereIn("event_id", data_get($data, "events"))->get();
-        
+
+        if ($startDate == $endDate) $endDate = Carbon::createFromFormat("Y-m-d H:i:s", "$endDate 00:00:00")->addDay(1)->setTimezone("-7")->format("Y-m-d H:i:s");
+        $startDate = Carbon::createFromFormat("Y-m-d H:i:s", "$startDate 00:00:00")->setTimezone("-7")->format("Y-m-d H:i:s");
+
+        $dataClientBookings = [];
+        $bookings = BookModel::with(["client", "seat"])->where([
+            ["created_at", ">=", $startDate],
+            ["created_at", "<", $endDate],
+            ["start_pending", null]
+        ])->whereIn("event_id", data_get($data, "events"))->get();
+        $events = EventModel::with(["ticketClasses"])->whereIn("id", $eventIds)->get();
+        $bookings->each(function ($booking) use (&$dataClientBookings) {
+            $bookingEventId = $booking->event_id;
+            $bookingSeatId = $booking->seat_id;
+            $ticketClassId = $this->getTicketClassOfSeat($bookingEventId, $bookingSeatId);
+            foreach ($dataClientBookings as &$data) {
+                if ($data["phone_number"] == $booking->client->phone_number || $data["id_number"] == $booking->client->id_number) {
+                    if (isset($data["events"][$bookingEventId])) {
+                        if (isset($data["events"][$bookingEventId][$ticketClassId])) {
+                            $data["events"][$bookingEventId][$ticketClassId][] = $booking->seat->name;
+                        } else {
+                            $data["events"][$bookingEventId][$ticketClassId] = [$booking->seat->name];
+                        }
+                    } else {
+                        $data["events"][$bookingEventId] = [
+                            $ticketClassId => [$booking->seat->name]
+                        ];
+                    }
+                    goto NEXT;
+                }
+            }
+            $dataClientBookings[$booking->client_id] = [
+                ...$booking->client->toArray(),
+                "events" => [
+                    $bookingEventId => [
+                        $ticketClassId => [$booking->seat->name]
+                    ]
+                ]
+            ];
+            NEXT:
+        });
+        dd($dataClientBookings);
         $this->aggregateRevenueDaily->export($bookings);
+    }
+
+    private function getTicketClassOfSeat($eventId, $seatId)
+    {
+        return EventSeatClassModel::where("event_id", $eventId)->where("seat_id", $seatId)->first()->ticket_class_id;
     }
 }
