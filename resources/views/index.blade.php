3db9fec2310dbf371907635245a4c7782b81f911
tuankiet116 <kietdat1612000@gmail.com>
2024-08-02 10:09:08 +0700

    update title

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>HGO Ticket Booking</title>
+        <title>Hanoi Ticket Booking</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
