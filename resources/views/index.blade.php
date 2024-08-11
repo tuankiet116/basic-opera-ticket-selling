7199a047fff1ac5e25158636fca5dfd97316eb7e
tuankiet116 <kietdat1612000@gmail.com>
2024-08-11 16:26:39 +0700

    feat: seo tags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Hanoi Ticket Booking</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link crossorigin="anonymous" href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap"
-            rel="stylesheet" />
-        <link crossorigin="anonymous" rel="stylesheet" href="https://unpkg.com/vue-select@latest/dist/vue-select.css">
-        <link crossorigin="anonymous" rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link crossorigin="anonymous"
-            href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-            rel="stylesheet">
-        @vite(['resources/js/index.js', 'resources/css/app.scss'])
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-25DTHZ7ESN"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
+    <title>Hanoi Ticket Booking</title>
+    <meta name="title" content="Hanoi Ticket">
+    <meta property="og:site_name" content="Hanoi Ticket">
+    <meta property="article:publisher" content="https://www.facebook.com/hoguomoperavn">
+    <meta name="description" content="Nhà hát Hồ Gươm">
+    <meta name="keywords"
+        content="hanoi,hgo,hoguomopera,hoguom,opera,nhà hát,hồ gươm,nhà hát hồ gươm, đặt vé, mua vé, sự kiện">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="revisit-after" content="1 days">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link crossorigin="anonymous" href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap"
+        rel="stylesheet" />
+    <link crossorigin="anonymous" rel="stylesheet" href="https://unpkg.com/vue-select@latest/dist/vue-select.css">
+    <link crossorigin="anonymous" rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link crossorigin="anonymous"
+        href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    @vite(['resources/js/index.js', 'resources/css/app.scss'])
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-25DTHZ7ESN"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
 
             function gtag() {
                 dataLayer.push(arguments);
@@ -29,11 +38,11 @@
             gtag('js', new Date());
 
             gtag('config', 'G-25DTHZ7ESN');
-        </script>
-    </head>
+    </script>
+</head>
 
-    <body>
-        <div id="app"></div>
-    </body>
+<body>
+    <div id="app"></div>
+</body>
 
 </html>
