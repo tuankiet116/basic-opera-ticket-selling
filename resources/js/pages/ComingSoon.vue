bdfbfcf9702757811061322d0091b68b931d2cd6
tuankiet116 <kietdat1612000@gmail.com>
2024-10-12 13:04:54 +0700

    update comming soon content

--- resources/js/pages/ComingSoon.vue
+++ resources/js/pages/ComingSoon.vue
@@ -1,25 +1,19 @@
 <template>
     <div class="container-fluid">
-        <div class="row py-5">
-            <div class="col-md-4 col-12 d-md-block d-none">
-                <img class="img-thumbnail" src="../../images/comingsoon.jpg" />
-            </div>
-            <div class="col-md-8 col-12 d-flex flex-column align-items-center justify-content-center">
+        <div class="row py-5 text-center">
+            <div class="col-12 d-flex flex-column align-items-center justify-content-center">
                 <div class="d-lg-block d-none">
-                    <p class="text-center fw-medium fs-2 mb-0">THE MOZART CONCERT </p>
-                    <p class="text-center fw-bold" style="font-size: 45px">Hẹn gặp lại vào hôm sau!</p>
+                    <p class="text-center fw-bold" style="font-size: 30px">Hẹn gặp lại vào hôm sau!</p>
+                    <p class="text-danger fw-semibold">Lịch hoạt động từ 10h sáng đến 22h tối hàng ngày</p>
                 </div>
                 <div class="d-md-block d-none d-lg-none">
-                    <p class="text-center fw-medium fs-3 mb-0">THE MOZART CONCERT</p>
-                    <p class="text-center fw-bold" style="font-size: 37px;">Hẹn gặp lại vào hôm sau!</p>
+                    <p class="text-center fw-bold" style="font-size: 27px;">Hẹn gặp lại vào hôm sau!</p>
+                    <p class="text-danger fw-semibold">Lịch hoạt động từ 10h sáng đến 22h tối hàng ngày</p>
                 </div>
                 <div class="d-md-none d-block">
-                    <p class="text-center fw-medium fs-4 mb-0">THE MOZART CONCERT</p>
-                    <p class="text-center fw-bold" style="font-size: 30px;">Hẹn gặp lại vào hôm sau!</p>
+                    <p class="text-center fw-bold" style="font-size: 25px;">Hẹn gặp lại vào hôm sau!</p>
+                    <p class="text-danger fw-semibold">Lịch hoạt động từ 10h sáng đến 22h tối hàng ngày</p>
                 </div>
-            </div>
-            <div class="col-md-4 col-12 d-md-none d-block">
-                <img class="img-thumbnail" src="../../images/comingsoon.jpg" />
             </div>
         </div>
     </div>
