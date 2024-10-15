54dfce1afa1cbe0a9a657618155504c90ae85fbc
tuankiet116 <kietdat1612000@gmail.com>
2024-10-15 13:43:53 +0700

    feat: update development admin account

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -13,10 +13,18 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            "name" => "Admin",
-            "email" => "live.hoguomopera@gmail.com",
-            "password" => bcrypt("FourSeasons@Hoguom.Opera")
-        ]);
+        if (config("app.env") == "development") {
+            DB::table('users')->insert([
+                "name" => "Admin",
+                "email" => "development@gmail.com",
+                "password" => bcrypt("development@123")
+            ]);
+        } else {
+            DB::table('users')->insert([
+                "name" => "Admin",
+                "email" => "live.hoguomopera@gmail.com",
+                "password" => bcrypt("FourSeasons@Hoguom.Opera")
+            ]);
+        }
     }
 }
